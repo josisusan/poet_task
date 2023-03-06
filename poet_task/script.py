941e27b46cfd909aff39f7f826a74dc757e6d93a
Susan Joshi <susan.joshi@cloudfactory.com>
2023-03-06 18:07:19 +0000

    Initial commit.

---
+++ poet_task/script.py
@@ -0,0 +1,11 @@
+from emoji import emojize
+
+def main():
+    print(emojize('Great Work :red_heart:'))
+
+
+def hello_world():
+    print(emojize('Hello World :green_heart:'))
+
+if __name__ == "__main__":
+    main()
